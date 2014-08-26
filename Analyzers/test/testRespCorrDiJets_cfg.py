0533fe04f6b5387d2fa6778731caa177e00ea2e5
David <sheffield@physics.rutgers.edu>
2014-08-26 15:43:16 -0500

    gets geometry

--- Analyzers/test/testRespCorrDiJets_cfg.py
+++ Analyzers/test/testRespCorrDiJets_cfg.py
@@ -6,32 +6,34 @@ process.load('FWCore.MessageService.MessageLogger_cfi')
 
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-process.GlobalTag.globaltag = 'START53_V19D::All' #'GR_R_52_V9::All' # not sure what tag to use
+from Configuration.AlCa.autoCond import autoCond
+process.load("Configuration.Geometry.GeometryIdeal_cff")
+process.GlobalTag.globaltag=autoCond['startup']
 
 #load the response corrections calculator
 process.load('HcalClosureTest.Analyzers.calcrespcorrdijets_cfi')
 process.load('JetMETCorrections.Configuration.JetCorrectionProducers_cff')
 process.load('JetMETCorrections.Configuration.JetCorrectionServices_cff')
-#process.load('JetMETCorrections.Configuration.L2L3Corrections_Summer09_cff')
 
 # run over files
 
-process.calcrespcorrdijets.rootHistFilename = cms.string('plots.root')
+process.calcrespcorrdijets.rootHistFilename = cms.string('Trees/test.root')
+#process.calcrespcorrdijets.doCaloJets = cms.bool(False)
+#process.calcrespcorrdijets.doPFJets = cms.bool(False)
+#process.calcrespcorrdijets.doGenJets = cms.bool(False)
+#process.calcrespcorrdijets.debug = cms.untracked.bool(True)
 
-###import FWCore.Python.FileUtils as FileUtils
-#readFiles = cms.untracked.vstring( FileUtils.loadListFromFile ('QCD_Pt30.list') )
+#import FWCore.Python.FileUtils as FileUtils
+#readFiles = cms.untracked.vstring( FileUtils.loadListFromFile ('Pion_Pt-50.list') )
 #process.source = cms.Source ("PoolSource",fileNames = readFiles)
 process.source = cms.Source("PoolSource",
-                            # replace 'myfile.root' with the source file you want to use
                             fileNames = cms.untracked.vstring(
-    #'/store/mc/Summer12/QCD_Pt-1400to1800_TuneZ2star_8TeV_pythia6/AODSIM/PU_S7_START52_V9-v1/0000/009E226E-F499-E111-B2BD-0025904B12FC.root'
-    #'file:SinglePiPt100_cfi_py_GEN_FASTSIM_HLT_VALIDATION.root'
-    'file:myfile.root'
+    'root://cmsxrootd-site.fnal.gov//store/mc/Summer12_DR53X/QCD_Pt-15to3000_TuneD6T_Flat_8TeV_pythia6/GEN-SIM-RECO/PU_S10_START53_V7A-v2/0000/001288D3-2AE3-E111-A943-0030487D5E5F.root'
     )
                             )
 #print readFiles
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.MessageLogger.cerr.FwkReport.reportEvery=cms.untracked.int32(1000)
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
