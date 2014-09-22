4f5a47b4d407424950c6ca7e0f1863769b755c0f
David Sheffield <sheffield@physics.rutgers.edu>
2014-09-21 22:37:57 -0500

    Prepare for CRAB jobs.

--- Analyzers/test/testRespCorrDiJets_cfg.py
+++ Analyzers/test/testRespCorrDiJets_cfg.py
@@ -17,8 +17,8 @@ process.load('JetMETCorrections.Configuration.JetCorrectionServices_cff')
 
 # run over files
 
-process.calcrespcorrdijets.rootHistFilename = cms.string('Trees/test.root')
-#process.calcrespcorrdijets.doCaloJets = cms.bool(False)
+process.calcrespcorrdijets.rootHistFilename = cms.string('tree.root')
+process.calcrespcorrdijets.doCaloJets = cms.bool(False)
 #process.calcrespcorrdijets.doPFJets = cms.bool(False)
 #process.calcrespcorrdijets.doGenJets = cms.bool(False)
 #process.calcrespcorrdijets.debug = cms.untracked.bool(True)
@@ -26,11 +26,8 @@ process.calcrespcorrdijets.rootHistFilename = cms.string('Trees/test.root')
 #import FWCore.Python.FileUtils as FileUtils
 #readFiles = cms.untracked.vstring( FileUtils.loadListFromFile ('Pion_Pt-50.list') )
 #process.source = cms.Source ("PoolSource",fileNames = readFiles)
-process.source = cms.Source("PoolSource",
-                            fileNames = cms.untracked.vstring(
-    'root://cmsxrootd-site.fnal.gov//store/mc/Summer12_DR53X/QCD_Pt-15to3000_TuneD6T_Flat_8TeV_pythia6/GEN-SIM-RECO/PU_S10_START53_V7A-v2/0000/001288D3-2AE3-E111-A943-0030487D5E5F.root'
-    )
-                            )
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring())
+
 #print readFiles
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
