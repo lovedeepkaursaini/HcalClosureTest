41e15f7b2b8429d0976c52087f5ec6b742b57f16
Andrius Juodagalvis <andjuo@hotmail.com>
2014-10-25 12:46:11 +0200

    Analyzers/test/get_few_events.py

---
+++ Analyzers/test/get_few_events.py
@@ -0,0 +1,30 @@
+# copy a sample of data file
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("MYCOPY")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+
+gjetsFiles = cms.untracked.vstring(
+  '/store/relval/CMSSW_5_3_14/RelValPhotonJets_Pt_10/GEN-SIM-RECO/START53_LV6-v1/00000/5847CE87-FB60-E311-A45A-0025905A6134.root',
+  '/store/relval/CMSSW_5_3_14/RelValPhotonJets_Pt_10/GEN-SIM-RECO/START53_LV6-v1/00000/C82EE6E4-4D60-E311-8621-0025905A4964.root'
+)
+
+qcdFiles = cms.untracked.vstring(
+ '/store/relval/CMSSW_5_3_14/RelValQCD_FlatPt_15_3000/GEN-SIM-RECO/START53_LV3_Feb14-v1/00000/D2D13174-A495-E311-8372-0025905A60F4.root',
+ '/store/relval/CMSSW_5_3_14/RelValQCD_FlatPt_15_3000/GEN-SIM-RECO/START53_LV3_Feb14-v1/00000/EAB6B353-9B95-E311-B000-00304867915A.root'
+)
+
+process.source = cms.Source("PoolSource",
+#                            fileNames = cms.untracked.vstring(
+#        '/store/mc/Summer12_DR53X/G_Pt-15to3000_TuneZ2_Flat_8TeV_pythia6/AODSIM/PU_S10_START53_V7A-v1/00000/0035BB1E-950E-E211-B5FE-002481E101DA.root'
+#        )
+                            fileNames= gjetsFiles
+                            #fileNames= qcdFiles
+)
+
+process.copyAll = cms.OutputModule("PoolOutputModule",
+          fileName = cms.untracked.string("selection.root") )
+process.printEventNumber = cms.OutputModule("AsciiOutputModule")
+process.p = cms.EndPath(process.copyAll + process.printEventNumber)
